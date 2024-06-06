ffc0e73a63e2cf8c9c7b7a436948b4ce42a7bcc7
Vishwa2612 <133838096+Vishwa2612@users.noreply.github.com>
2024-06-06 23:59:21 +0530

    Contact.tsx

--- src/app/Components/Contact.tsx
+++ src/app/Components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/lbkmqgrb" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <h1 className='text-4xl font-bold inline border-b-4 border-green-500 text-white'>Contact Me</h1>
-                <p className='text-white py-4'>// Feel free to drop a message or email me at example@domain.com</p>
+                <p className='text-white py-4'>// Feel free to drop a message or email me at vishwakumar26122003@gmail.com</p>
             </div>
             <input className='bg-gray-200 p-2 rounded-md' type="text" placeholder='Name' name='name' required />
             <input className='my-4 p-2 bg-gray-200 rounded-md' type="email" placeholder='Email' name='email' required />
